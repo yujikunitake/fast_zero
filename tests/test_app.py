e38cea9023164d10099a537e72663be225206ccf
Yuji Miranda Kunitake <andrekuni123@gmail.com>
2025-04-03 23:17:44 -0300

    iniciando o projeto

---
+++ tests/test_app.py
@@ -0,0 +1,16 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from fast_zero.app import app
+
+client = TestClient(app)
+
+
+def test_read_root_deve_retornar_ok_e_ola_mundo():
+    client = TestClient(app)  # Arrange (Organização)
+
+    response = client.get('/')  # Act (Ação)
+
+    assert response.status_code == HTTPStatus.OK  # Assert (Afirmação)
+    assert response.json() == {'message': 'Olá Mundo!'}
